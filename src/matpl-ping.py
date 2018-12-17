1cf6a932b2a9a874eddd34d20b73ff7da54cc15e
im <morigo.2015@gmail.com>
2018-12-18 00:14:32 +0200

    heatmap

--- src/matpl-ping.py
+++ src/matpl-ping.py
@@ -3,8 +3,10 @@ import time
 import sys
 from typing import List, Dict, Tuple
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 from matplotlib import cm
+from matplotlib.colors import LinearSegmentedColormap
 
 import numpy as np
 import pandas as pd
@@ -27,21 +29,29 @@ host_info = {
 
 PandasFrame = pd.core.frame.DataFrame  # shortcut for annotations
 
+# @formatter:off
+grade_info = [
+    {'level': 'disconnect', 'left': -np.inf, 'right': -1,     'color_scale': [0.0, 'red']},
+    {'level': 'poor',       'left': 7,       'right': np.inf, 'color_scale': [0.3, 'orange']},
+    {'level': 'moderate',   'left': 5,       'right': 7,      'color_scale': [0.6, 'rgb(235,206,135)']},
+    {'level': 'good',       'left': -1,      'right': 5,      'color_scale': [1.0, 'green']},
+]
+# @formatter:on
+
+def _set_grade(df: PandasFrame):
+    """ avg_rtt --> grade
+    """
+    df['grade'] = pd.Series(index=df.index)
+    for gr in grade_info:
+        df.loc[(gr['left'] < df.avg_rtt) & (df.avg_rtt <= gr['right']), 'grade'] = gr['color_scale'][0]
+    print('grade: ', df['grade'].value_counts())
+
 
 # noinspection PyUnresolvedReferences
-class _PltPingNotUpdatable:
+class PltPing:
     """
-    plot on plot.ly incrementally: init/update chart
+    plot charts for ping data
     """
-    # @formatter:off
-    grade_info = [
-        {'level': 'disconnect', 'left': -np.inf, 'right': -1,     'color_scale': [0.0, 'red']},
-        {'level': 'poor',       'left': 7,       'right': np.inf, 'color_scale': [0.3, 'orange']},
-        {'level': 'moderate',   'left': 5,       'right': 7,      'color_scale': [0.6, 'rgb(235,206,135)']},
-        {'level': 'good',       'left': -1,      'right': 5,      'color_scale': [1.0, 'green']},
-    ]
-    # @formatter:on
-
     def __init__(self):
         self.db = MyDb()  # connection to MySQL
 
@@ -49,79 +59,6 @@ class _PltPingNotUpdatable:
         query = 'select * from ping order by time asc'
         df = pd.read_sql(query, self.db.mydb)
         return df
-
-    @classmethod
-    def _set_grade(cls, df: PandasFrame):
-        """ avg_rtt --> grade
-        """
-        df['grade'] = pd.Series(index=df.index)
-        for gr in cls.grade_info:
-            df.loc[(gr['left'] < df.avg_rtt) & (df.avg_rtt <= gr['right']), 'grade'] = gr['color_scale'][0]
-        print('grade: ', df['grade'].value_counts())
-
-    # @classmethod
-    # def _prepare_data_heatmap(cls, df: PandasFrame) -> PandasFrame:
-    #     df.index = df['time']
-    #     # del df['time']
-    #     df.index = df.index.floor('1T')  # truncate to minutes
-    #
-    #     # avg_rtt --> grades
-    #     cls._set_grade(df)
-    #
-    #     # hosts --> columns
-    #     df = df.groupby([df.index, 'host'])['grade'].agg('mean').unstack()  # minutes * hosts [avg_rtt]  avg_rtt
-    #     globals()['dfm'] = df.copy()
-    #
-    #     # df = df.resample('1H').mean()
-    #     # globals()['dfh']=df.copy()
-    #
-    #     # sort columns by seqn from host_info
-    #     df.rename(lambda s: f'{host_info[s][1]:02d}.{s}', axis='columns', inplace=True)  # host -> <seqn>.host
-    #     df.sort_index(axis=1, inplace=True)  # sort by seqn
-    #     df.rename(lambda s: host_info[s[3:]][0], axis='columns', inplace=True)  # <seqn>.host -> host_name
-    #     return df
-    #
-    # @classmethod
-    # def _create_heatmap(cls, df):
-    #     print('grade info: ', [gr['color_scale'] for gr in cls.grade_info])
-    #     data = [
-    #         go.Heatmap(
-    #             z=df.T.values.tolist(),
-    #             x=df.index.tolist(),
-    #             y=df.columns,
-    #             colorscale=[gr['color_scale'] for gr in cls.grade_info],
-    #             ygap=4,
-    #             xgap=1
-    #         )
-    #     ]
-    #     layout = go.Layout(
-    #         title='ping stat',
-    #         # xaxis=dict(ticks=''),  # nticks=12),
-    #         xaxis=dict(
-    #             rangeselector=dict(
-    #                 buttons=list([
-    #                     dict(count=1, label='1 day', step='day', stepmode='backward'),
-    #                     dict(count=1, label='1 hour', step='hour', stepmode='backward'),
-    #                     dict(count=1, label='1 minute', step='minute', stepmode='backward'),
-    #                     dict(step='all')
-    #                 ])
-    #             ),
-    #             rangeslider=dict(visible=True),
-    #             type='date'
-    #         ),
-    #         yaxis=dict(ticks='')
-    #     )
-    #     fig = go.Figure(data=data, layout=layout)
-    #     return fig
-    #
-    # def draw_heat(self):
-    #     """ overwrite chart based on history
-    #     """
-    #     df = self._get_ping_history()
-    #     df_p = self._prepare_data_heatmap(df)
-    #     fig = self._create_heatmap(df_p)
-    #     r = py.iplot(fig, filename=chart_name, fileopt='overwrite')
-    #     print(f'plot created at {r.resource}.')
 
     @classmethod
     def _prepare_data(cls, df: PandasFrame):
@@ -152,22 +89,19 @@ class _PltPingNotUpdatable:
 
         # hosts --> columns
         df = df.groupby([df.index, 'host'])['avg_rtt'].agg('mean').unstack()  # minutes * hosts [avg_rtt]  avg_rtt
+        # hosts --> host_names
+        df.columns = [host_info[d][0] for d in df.columns]
         globals()['dfm'] = df.copy()
 
         # @formatter:off
         scales = [
-            {'period': 'hour',  'resample_step': None, 'ticks': 60},
-            {'period': 'day',   'resample_step': '1H', 'ticks': 24},
-            {'period': 'month', 'resample_step': '1D', 'ticks': 30},
+            {'period': 'hour',  'resample_step': None, 'ticks': 60, 'format':"%H:%M",    'nticks':30, 'xloc_base':3.0},
+            {'period': 'day',   'resample_step': '1H', 'ticks': 24, 'format':"%Hh",      'nticks':30, 'xloc_base':1.0},
+            {'period': 'month', 'resample_step': '1D', 'ticks': 30, 'format':"%Y-%m-%d", 'nticks':30, 'xloc_base':1.0},
         ]
         # @formatter:on
 
-        # fig = plt.figure(1) #, figsize=(9, 3))
-
         for ind, sc in enumerate(scales):
-            # ax = plt.subplot(3, 1, ind + 1)
-            fig,ax = plt.subplots()
-            plt.title(sc['period'])
             if sc['resample_step']:
                 df2 = df.resample(sc['resample_step']).mean()
             else:
@@ -175,24 +109,35 @@ class _PltPingNotUpdatable:
             df2 = df2.tail(sc['ticks'])
             globals()[f"df_{sc['period']}"] = df2.copy()
 
-            # fig, ax = plt.subplots()
-            # cmap=cm.get_cmap('viridis',128)
+            fig, ax = plt.subplots()
+            ax.set_title(f"Last {sc['period']}")
             heatmap = ax.pcolor(df2.T,  # cmap=cmap,  #plt.cm.viridis,
+                                cmap=LinearSegmentedColormap.from_list("", ["green", "olive", "darkkhaki", "orange"]),
                                 vmin=np.nanmin(df2.T), vmax=np.nanmax(df2.T),
                                 edgecolors='k', linewidth=1)  # cm.get_cmap('viridis', 256))
             ax.patch.set(color='red')  # hatch='x',edgecolor='red',fill=True,
-            # heatmap.cmap.set_under('red')
             fig.colorbar(heatmap)  # , extend='both')
 
-            plt.xticks(np.arange(0.5, len(df2.index), 1), df2.index,
-                       rotation='vertical')  # ,verticalalignment='bottom')
-            plt.yticks(np.arange(0.5, len(df2.columns), 1), df2.columns)
+            # set x axis
+            ax.xaxis.set_major_locator(ticker.IndexLocator(sc['xloc_base'], 0.5))
+            xformatter = lambda x, pos: f"{df2.index[np.clip(int(x), 0, len(df2.index) - 1)].strftime(sc['format'])}"
+            ax.xaxis.set_major_formatter(ticker.FuncFormatter(xformatter))
+            ax.tick_params(axis='x', labelrotation=45.)
+
+            # set y axis
+            ax.yaxis.set_major_locator(ticker.IndexLocator(1.0, 0.5))
+            yformatter = lambda x, pos: f"{df2.columns[int(x)] if x < len(df2.columns) else '=No label='}"
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(yformatter))
+
+            # show/save result
+            plt.tight_layout()
             plt.savefig(fname=f"/home/im/mypy/pmon/tst/matpl-{sc['period']}.png")
             plt.show()
+            globals()['a'] = ax
 
 
 # noinspection PyUnresolvedReferences
-class PltPing(_PltPingNotUpdatable):
+class PltPingUpdatable(PltPing):
     """ Plot charts, based on MySQL table ping. Allows to update chart based on ping table updates, too.
     """
 
@@ -235,7 +180,7 @@ class PltPing(_PltPingNotUpdatable):
 
 
 if __name__ == '__main__':
-    p = _PltPingNotUpdatable()
+    p = PltPing()
     p.draw_heat()
     # p.draw_lines()
     #
