a669b53b8247276996e846fa41180c80c59bf8d9
im <morigo.2015@gmail.com>
2018-12-18 14:48:15 +0200

    heatmap - small fixes

--- src/matpl-ping.py
+++ src/matpl-ping.py
@@ -29,18 +29,18 @@ host_info = {
 
 PandasFrame = pd.core.frame.DataFrame  # shortcut for annotations
 
-# @formatter:off
-grade_info = [
-    {'level': 'disconnect', 'left': -np.inf, 'right': -1,     'color_scale': [0.0, 'red']},
-    {'level': 'poor',       'left': 7,       'right': np.inf, 'color_scale': [0.3, 'orange']},
-    {'level': 'moderate',   'left': 5,       'right': 7,      'color_scale': [0.6, 'rgb(235,206,135)']},
-    {'level': 'good',       'left': -1,      'right': 5,      'color_scale': [1.0, 'green']},
-]
-# @formatter:on
 
 def _set_grade(df: PandasFrame):
     """ avg_rtt --> grade
     """
+    # @formatter:off
+    grade_info = [
+        {'level': 'disconnect', 'left': -np.inf, 'right': -1,     'color_scale': [0.0, 'red']},
+        {'level': 'poor',       'left': 7,       'right': np.inf, 'color_scale': [0.3, 'orange']},
+        {'level': 'moderate',   'left': 5,       'right': 7,      'color_scale': [0.6, 'rgb(235,206,135)']},
+        {'level': 'good',       'left': -1,      'right': 5,      'color_scale': [1.0, 'green']},
+    ]
+    # @formatter:on
     df['grade'] = pd.Series(index=df.index)
     for gr in grade_info:
         df.loc[(gr['left'] < df.avg_rtt) & (df.avg_rtt <= gr['right']), 'grade'] = gr['color_scale'][0]
@@ -52,6 +52,7 @@ class PltPing:
     """
     plot charts for ping data
     """
+
     def __init__(self):
         self.db = MyDb()  # connection to MySQL
 
@@ -64,10 +65,14 @@ class PltPing:
     def _prepare_data(cls, df: PandasFrame):
         df.index = df['time']
         df.index = df.index.floor('1T')  # truncate to minutes
-
+        # avg==-1 --> NaN
         df.avg_rtt.clip_upper(15.0, inplace=True)
         df.loc[df.avg_rtt == -1, 'avg_rtt'] = np.nan
-
+        # hosts --> columns
+        df = df.groupby([df.index, 'host'])['avg_rtt'].agg('mean').unstack()  # minutes * hosts [avg_rtt]  avg_rtt
+        # hosts --> host_names
+        df.columns = [host_info[d][0] for d in df.columns]
+        globals()['dfm'] = df.copy()
         return df
 
     def draw_lines(self):
@@ -83,15 +88,7 @@ class PltPing:
     def draw_heat(self):
         df = self._get_ping_history()
         df = self._prepare_data(df)
-
-        # avg_rtt --> grades
-        # self._set_grade(df)
-
-        # hosts --> columns
-        df = df.groupby([df.index, 'host'])['avg_rtt'].agg('mean').unstack()  # minutes * hosts [avg_rtt]  avg_rtt
-        # hosts --> host_names
-        df.columns = [host_info[d][0] for d in df.columns]
-        globals()['dfm'] = df.copy()
+        # self._set_grade(df) # avg_rtt --> grades
 
         # @formatter:off
         scales = [
@@ -100,21 +97,18 @@ class PltPing:
             {'period': 'month', 'resample_step': '1D', 'ticks': 30, 'format':"%Y-%m-%d", 'nticks':30, 'xloc_base':1.0},
         ]
         # @formatter:on
-
         for ind, sc in enumerate(scales):
-            if sc['resample_step']:
-                df2 = df.resample(sc['resample_step']).mean()
-            else:
-                df2 = df.copy()
-            df2 = df2.tail(sc['ticks'])
+            df2 = df.resample(sc['resample_step']).mean() if sc['resample_step'] else df.copy()  # aggregate if need
+            df2 = df2.tail(sc['ticks'])  # cut data out of reporting period
+
             globals()[f"df_{sc['period']}"] = df2.copy()
 
             fig, ax = plt.subplots()
             ax.set_title(f"Last {sc['period']}")
-            heatmap = ax.pcolor(df2.T,  # cmap=cmap,  #plt.cm.viridis,
+            heatmap = ax.pcolor(df2.T, # cm.get_cmap('viridis', 256))
                                 cmap=LinearSegmentedColormap.from_list("", ["green", "olive", "darkkhaki", "orange"]),
                                 vmin=np.nanmin(df2.T), vmax=np.nanmax(df2.T),
-                                edgecolors='k', linewidth=1)  # cm.get_cmap('viridis', 256))
+                                edgecolors='k', linewidth=1)
             ax.patch.set(color='red')  # hatch='x',edgecolor='red',fill=True,
             fig.colorbar(heatmap)  # , extend='both')
 
@@ -136,56 +130,6 @@ class PltPing:
             globals()['a'] = ax
 
 
-# noinspection PyUnresolvedReferences
-class PltPingUpdatable(PltPing):
-    """ Plot charts, based on MySQL table ping. Allows to update chart based on ping table updates, too.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self._last_timestamp: datetime.datetime = None  # last timestamp been sent to chart server
-
-    def _update_last_timestamp(self, df: PandasFrame):
-        t = df[-1:]['time']
-        self._last_timestamp = t.dt.to_pydatetime()[0]
-        print(f'last timestamp = {self._last_timestamp}')
-
-    def _get_ping_history(self) -> PandasFrame:
-        df = super()._get_ping_history()
-        self._update_last_timestamp(df)
-        return df
-
-    def _get_ping_newdata(self) -> PandasFrame:
-        """ get new records from ping """
-        last_time = MyDb.datetime_2_sql(self._last_timestamp)
-        query = f"""select * from ping where time > str_to_date('{last_time}','%Y-%m-%d %T') order by time asc"""
-        # print('query: ', query)
-        df = pd.read_sql(query, self.db.mydb)
-        self._update_last_timestamp(df)
-        return df
-
-    def start(self):
-        """ overwrite chart based on all previous history """
-        super().draw()
-
-    def update(self):
-        """ update chart based on last data (inserted after last update)
-        """
-        df = self._get_ping_newdata()
-        df = self._prepare_data_heatmap(df)
-        print('update:  df.shape=', df.shape)
-        fig = self._create_heatmap(df)
-        r = py.iplot(fig, filename=chart_name, fileopt='extend')
-        print(f'       =========== {df.shape[0]} timemarks added to {r.resource}')
-
-
 if __name__ == '__main__':
     p = PltPing()
     p.draw_heat()
-    # p.draw_lines()
-    #
-    # iter_numb = 0
-    # for iter_ in range(iter_numb):
-    #     print(f'iter={iter_}/{iter_numb}')
-    #     time.sleep(100)
-    #     plt.update()
